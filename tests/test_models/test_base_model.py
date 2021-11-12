5c2d3cc370d35eaf4bc141bea3b254e32e01a200
krummitz <amidbidee@gmail.com>
2021-11-12 23:44:04 +0100

    test complete

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,12 +17,8 @@ class Test_BaseModel(unittest.TestCase):
         """set up TestCase with some models and args
         """
         self.model1 = BaseModel()
-        test_args = {'created_at': datetime(2021, 11, 10, 2, 6, 55, 258849),
-                     'updated_at': datetime(2021, 11, 10, 2, 6, 55, 258966),
-                     'id': '46458416-e5d5-4985-aa48-a2b369d03d2a',
-                     'name': 'model1'}
-        self.model2 = BaseModel(test_args)
-        self.model2.save()
+        self.model1.name = 'First model'
+        self.model1.number = 8
 
     def test_instantiation(self):
         """test __init__ method
@@ -32,30 +28,41 @@ class Test_BaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.model1, "id"))
         self.assertFalse(hasattr(self.model1, 'updated_at'))
 
-    def test_reinstantiation(self):
-        self.assertIsInstance(self.model2, BaseModel)
-        self.assertEqual(
-            self.model2.id, '46458416-e5d5-4985-aa48-a2b369d03d2a')
-        self.assertEqual(self.model2.created_at, datetime(
-            2021, 11, 10, 2, 6, 55, 258849))
-
+    
     def test_save(self):
-        self.assertFalse(hasattr(self.model1, "updated_at"))
         self.model1.save()
         self.assertTrue(hasattr(self.model1, "updated_at"))
-        old_time = self.model2.updated_at
-        self.model2.save()
-        self.assertNotEqual(old_time, self.model2.updated_at)
+        old_time = self.model1.updated_at
+        self.model1.save()
+        self.assertNotEqual(old_time, self.model1.updated_at)
 
-    def test_to_json(self):
-        serialized = self.model2.to_json()
-        self.assertNotEqual(self.model2.__dict__, serialized)
-        self.assertNotIsInstance(serialized['created_at'], datetime)
-        self.assertNotIsInstance(serialized['updated_at'], datetime)
-        self.assertEqual(serialized['created_at'],
-                         '2021-11-10 02:06:55.258849')
-        self.assertTrue(hasattr(serialized, '__class__'))
-        self.assertEqual(serialized['__class__'], 'BaseModel')
+    def test_to_dict(self):
+        self.model1.save()
+        created_at_datetime = self.model1.created_at
+        self.test_args = self.model1.to_dict()
+        self.assertTrue(
+            ('id' and 'created_at' and 'updated_at' and 'name' and '__class__' and 'number') in self.test_args.keys())
+        self.assertIsInstance(self.test_args['id'], str)
+        self.assertIsInstance(self.test_args['__class__'], str)
+        self.assertIsInstance(self.test_args['created_at'], str)
+        self.assertIsInstance(self.test_args['updated_at'], str)
+        self.assertIsInstance(self.test_args['name'], str)
+        self.assertIsInstance(self.test_args['number'], int)
+        self.assertEqual(datetime.isoformat(created_at_datetime), self.test_args['created_at'])
+
+    def test_reinstantiation(self):
+        test_args = self.model1.to_dict()
+        self.model2 = BaseModel(**test_args)
+        self.model2.save()
+
+        self.assertIsInstance(self.model2, BaseModel)
+        self.assertIsInstance(self.model2.created_at, datetime)
+        self.assertIsInstance(self.model2.updated_at, datetime)
+        self.assertFalse('__class__' in self.model2.__dict__)
+        self.assertEqual(test_args['id'], self.model2.id)
+        self.assertEqual(datetime.fromisoformat(test_args['created_at']), self.model2.created_at)
+        self.assertEqual(test_args['name'], self.model2.name)
+        self.assertTrue(test_args['number'], self.model2.number)
 
 
 if __name__ == '__main__':
